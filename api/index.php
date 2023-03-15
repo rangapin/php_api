4c7be36612e1b52365281ebefa58c2c2f9250527
rangapin <rangapin@gmx.com>
2023-03-15 17:33:57 +0100

    added the api framework

---
+++ api/index.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+set_exception_handler("ErrorHandler::handleException");
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+    
+    http_response_code(404);
+    exit;
+}
+
+header("Content-type: application/json; charset=UTF-8");
+
+$controller = new TaskController;
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
+
+
+
+
+
+
+
+
+
