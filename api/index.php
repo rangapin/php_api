de17febdc39b2656ceecef31fcd658d47902db74
rangapin <rangapin@gmx.com>
2023-03-17 15:31:59 +0100

    creating api

--- api/index.php
+++ api/index.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . "/vendor/autoload.php";
 
+set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
@@ -22,7 +26,11 @@ if ($resource != "tasks") {
 
 header("Content-type: application/json; charset=UTF-8");
 
-$controller = new TaskController;
+$database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+
+$task_gateway = new TaskGateway($database);
+
+$controller = new TaskController($task_gateway);
 
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
 
--- src/TaskController.php
+++ src/TaskController.php
@@ -2,34 +2,75 @@
 
 class TaskController
 {
+    public function __construct(private TaskGateway $gateway)
+    {
+    }
+    
     public function processRequest(string $method, ?string $id): void
     {
         if ($id === null) {
             
             if ($method == "GET") {
                 
-                echo "index";
+                echo json_encode($this->gateway->getAll());
+                
             } elseif ($method == "POST") {
                 
-                echo "create";
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+                
+                $errors = $this->getValidationErrors($data);
+                
+                if ( ! empty($errors)) {
+                    
+                    $this->respondUnprocessableEntity($errors);
+                    return;
+                    
+                }
+                
+                $id = $this->gateway->create($data);
+                
+                $this->respondCreated($id);
+                
             } else {
                 
                 $this->respondMethodNotAllowed("GET, POST");
             }
         } else {
             
+            $task = $this->gateway->get($id);
+            
+            if ($task === false) {
+                
+                $this->respondNotFound($id);
+                return;
+            }
+            
             switch ($method) {
                 
                 case "GET":
-                    echo "show $id";
+                    echo json_encode($task);
                     break;
                 
                 case "PATCH":
-                    echo "update $id";
+                
+                    $data = (array) json_decode(file_get_contents("php://input"), true);
+                    
+                    $errors = $this->getValidationErrors($data, false);
+                    
+                    if ( ! empty($errors)) {
+                        
+                        $this->respondUnprocessableEntity($errors);
+                        return;
+                        
+                    }
+                
+                    $rows = $this->gateway->update($id, $data);
+                    echo json_encode(["message" => "Task updated", "rows" => $rows]);
                     break;
                     
                 case "DELETE":
-                    echo "delete $id";
+                    $rows = $this->gateway->delete($id);
+                    echo json_encode(["message" => "Task deleted", "rows" => $rows]);
                     break;
                     
                 default:
@@ -38,10 +79,50 @@ class TaskController
         }
     }
     
+    private function respondUnprocessableEntity(array $errors): void
+    {
+        http_response_code(422);
+        echo json_encode(["errors" => $errors]);
+    }
+    
     private function respondMethodNotAllowed(string $allowed_methods): void
     {
         http_response_code(405);
         header("Allow: $allowed_methods");
+    }
+    
+    private function respondNotFound(string $id): void
+    {
+        http_response_code(404);
+        echo json_encode(["message" => "Task with ID $id not found"]);
+    }
+    
+    private function respondCreated(string $id): void
+    {
+        http_response_code(201);
+        echo json_encode(["message" => "Task created", "id" => $id]);
+    }
+    
+    private function getValidationErrors(array $data, bool $is_new = true): array
+    {
+        $errors = [];
+        
+        if ($is_new && empty($data["name"])) {
+            
+            $errors[] = "name is required";
+            
+        }
+        
+        if ( ! empty($data["priority"])) {
+            
+            if (filter_var($data["priority"], FILTER_VALIDATE_INT) === false) {
+                
+                $errors[] = "priority must be an integer";
+                
+            }
+        }
+        
+        return $errors;
     }
 }
 
